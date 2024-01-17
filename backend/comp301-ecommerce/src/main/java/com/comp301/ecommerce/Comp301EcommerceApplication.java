02a4f94f235e120787b3e8b9bddca37a386106ed
berkezap <ihsanberke.ozsap@std.yeditepe.edu.tr>
2024-01-17 16:54:54 +0300

    test
    test

---
+++ backend/comp301-ecommerce/src/main/java/com/comp301/ecommerce/Comp301EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.comp301.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp301EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp301EcommerceApplication.class, args);
+	}
+
+}
---
+++ backend/comp301-ecommerce/src/test/java/com/comp301/ecommerce/Comp301EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp301.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp301EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
