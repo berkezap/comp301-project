fcf409724289d67cfaa6f9e0558a8a6db20d0448
berkezap <ihsanberke.ozsap@std.yeditepe.edu.tr>
2024-01-17 18:06:44 +0300

    sepet
    sepet

---
+++ frontend/angular-ecommerce/src/app/components/cart-status/cart-status.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-status',
+  templateUrl: './cart-status.component.html',
+  styleUrls: ['./cart-status.component.css'],
+})
+export class CartStatusComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
