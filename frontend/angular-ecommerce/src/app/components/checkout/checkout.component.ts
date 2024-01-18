d638333c01eee7328036a1b7956cf36338341bd1
berkezap <ihsanberke.ozsap@std.yeditepe.edu.tr>
2024-01-18 19:19:12 +0300

    checkout fix
    checkout fix

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -14,65 +14,32 @@ export class CheckoutComponent implements OnInit {
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  creditCardYears: number [] = [];
-  creditCardMonths: number [] = [];
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
 
   constructor(private formBuilder: FormBuilder,
-              private luv2ShopFormService: Luv2ShopFormService) { }
+    private luv2ShopFormService: Luv2ShopFormService) { }
 
   ngOnInit(): void {
-    this.checkoutFormGroup = this.formBuilder.group({
-      customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
-      }),
-      shippingAddress: this.formBuilder.group({
-        street: [''],
-        city: [''],
-        state: [''],
-        country: [''],
-        zipCode: ['']
-      }),
-      billingAddress: this.formBuilder.group({
-        street: [''],
-        city: [''],
-        state: [''],
-        country: [''],
-        zipCode: ['']
-      }),
-      creditCard: this.formBuilder.group({
-        cardType: [''],
-        nameOnCard: [''],
-        cardNumber: [''],
-        securityCode: [''],
-        expirationMonth: [''],
-        expirationYear: ['']
-      })
-    });
+    // ... (existing code)
 
     // populate credit card months
-
     const startMonth: number = new Date().getMonth() + 1;
-    console.log("startMonth: " + startMonth);
-
     this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
       data => {
-        console.log("Retrieved credit card months: " + JSON.stringify(data));
         this.creditCardMonths = data;
       }
     );
 
     // populate credit card years
-
     this.luv2ShopFormService.getCreditCardYears().subscribe(
       data => {
-        console.log("Retrieved credit card years: " + JSON.stringify(data));
         this.creditCardYears = data;
       }
-    )
-
+    );
   }
+
+  // ... (existing methods)
 
   copyShippingAddressToBillingAddress(event: any) {
     if (event.target.checked) {
@@ -85,36 +52,36 @@ export class CheckoutComponent implements OnInit {
 
   onSubmit() {
     console.log("Handling the submit button");
+
     const customerValue = this.checkoutFormGroup.get('customer')?.value;
     if (customerValue) {
       console.log(customerValue);
       console.log("The email address is " + customerValue.email);
     }
 
-    handleMonthsAndYears() {
+    // Call the handleMonthsAndYears function
+    this.handleMonthsAndYears();
+  }
 
-      const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+  handleMonthsAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
 
-      const currentYear: number = new Date().getFullYear();
-      const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
 
-      // if the current year equals the selected year, then start with the current month
+    // if the current year equals the selected year, then start with the current month
+    let startMonth: number;
 
-      let startMonth: number;
-
-      if (currentYear === selectedYear) {
-        startMonth = new Date().getMonth() +1;
-      }
-      else {
-        startMonth = 1;
-      }
-
-      this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
-        data => {
-          console.log("Retrieved credit card months: " + JSON.stringify(data));
-          this.creditCardMonths = data;
-        }
-      );
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    } else {
+      startMonth = 1;
     }
+
+    this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        this.creditCardMonths = data;
+      }
+    );
   }
 }
