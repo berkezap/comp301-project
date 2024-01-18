f0d2963a07fc9b169d028183fa4c3ab21c1fdc97
berkezap <ihsanberke.ozsap@std.yeditepe.edu.tr>
2024-01-18 23:11:22 +0300

    Repo
    created country and state repo

---
+++ backend/comp301-ecommerce/src/main/java/com/comp301/ecommerce/dao/CountryRepository.java
@@ -0,0 +1,11 @@
+package com.comp301.ecommerce.dao;
+
+import com.comp301.ecommerce.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("htpp://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
+public interface CountryRepository extends JpaRepository<Country,Integer> {
+}
